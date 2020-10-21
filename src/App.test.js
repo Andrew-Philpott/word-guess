d8b3280c9104a1dec385c70205d64f0e574f19c4
Andrew Philpott <andrewphilpott92@gmail.com>
2020-10-21 15:55:28 -0700

    Update react and react-dom to version 16 for enzyme testing.

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,9 @@
+import { mount } from "enzyme";
 import React from "react";
 import App from "./App";
+
+test("renders the app component correctly", () => {
+  const wrapper = mount(<App />);
+  const component = wrapper.find(`[data-test="component-app"]`);
+  expect(component.length).toBe(1);
+});
