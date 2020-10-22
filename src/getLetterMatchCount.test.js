8f85946c5cb5f443ca2399f9f6a8b5602f4ae488
Andrew Philpott <andrewphilpott92@gmail.com>
2020-10-21 18:11:01 -0700

    Add tests for getlettermatchcount function.

---
+++ src/getLetterMatchCount.test.js
@@ -0,0 +1,17 @@
+import getLetterMatchCount from "./getLetterMatchCount";
+
+describe("getLetterMatchCount", () => {
+  const secretWord = "party";
+  test("returns correct count when there are no matching letters", () => {
+    const count = getLetterMatchCount("bones", secretWord);
+    expect(count).toBe(0);
+  });
+  test("returns the correct count where there are 3 matching letters", () => {
+    const count = getLetterMatchCount("train", secretWord);
+    expect(count).toBe(3);
+  });
+  test("returns correct count when there are duplicate letters in the guess", () => {
+    const count = getLetterMatchCount("parka", secretWord);
+    expect(count).toBe(3);
+  });
+});
